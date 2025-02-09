b7ee6d4f837a1af37b0f69b43ed14501da6d79a7
unknown <clonefull81@gmail.com>
2025-02-09 23:11:54 +0700

    Update

---
+++ ChessBoardManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaroDemo
+{
+    public class ChessBoardManager
+    {
+        #region Properties
+        private Panel chessBoard;
+        public Panel ChessBoard
+        {
+            get { return chessBoard; }
+            set { chessBoard = value; }
+        }
+        #endregion
+        #region Initialize
+        public ChessBoardManager(Panel chessBoard)
+        {
+            this.chessBoard = chessBoard;
+        }
+        #endregion
+        #region Methods
+        public void DrawChessBoard()
+        {
+            Button oldButton = new Button() { Width = 0, Location = new Point(0, 0) };
+            for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++)
+            {
+                for (int j = 0; j < Cons.CHESS_BOARD_WIDTH; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = Cons.CHESS_WIDTH,
+                        Height = Cons.CHESS_HEIGHT,
+                        Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y)
+                    };
+                    ChessBoard.Controls.Add(btn);
+                    oldButton = btn;
+                }
+                oldButton.Location = new Point(0, oldButton.Location.Y + oldButton.Height);
+                oldButton.Width = 0;
+                oldButton.Height = 0;
+            }
+            #endregion
+
+        }
+    }
+}
