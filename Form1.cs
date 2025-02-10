f192f675362f6f79ee667574306f3de78b259730
unknown <clonefull81@gmail.com>
2025-02-10 07:11:07 +0700

    update1

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+namespace GameCaroDemo
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        ChessBoardManager ChessBoard;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            ChessBoard = new ChessBoardManager(pnlChessBoard);
+            ChessBoard.DrawChessBoard();
+
+        }
+        
+    }
+}
